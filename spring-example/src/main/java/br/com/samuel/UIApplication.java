a00173b211a6fc0e5a4666d2890b0225fed152e4
Samuel Manoel Silveira <samuelsilveiram@gmail.com>
2017-03-29 00:13:04 -0300

    Criação do projeto de resource

---
+++ spring-example/src/main/java/br/com/samuel/UIApplication.java
@@ -0,0 +1,63 @@
+package br.com.samuel;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class UIApplication {
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UIApplication.class, args);
+	}
+
+	@RequestMapping("/token")
+	public Map<String, String> token(HttpSession session) {
+		return Collections.singletonMap("token", session.getId());
+	}
+
+	// @Configuration
+	// @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	// protected static class SecurityConfiguration extends
+	// WebSecurityConfigurerAdapter {
+	// @Override
+	// protected void configure(HttpSecurity http) throws Exception {
+	// // @formatter:off
+	// http.formLogin().and().logout().and().authorizeRequests()
+	// .antMatchers("/index.html", "/home.html", "/login.html",
+	// "/").permitAll().anyRequest()
+	// .authenticated().and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	// // @formatter:on
+	// }
+	// }
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/")
+					.permitAll().anyRequest().authenticated().and().csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		}
+	}
+}
